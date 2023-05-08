82490c0facb51a2347976ac00fa07bb737e1ec7c
Eugene Kalinin <e.v.kalinin@gmail.com>
2023-05-08 15:28:48 +0300

    fix comment style

--- src/utils/fsUtils.ts
+++ src/utils/fsUtils.ts
@@ -1,8 +1,6 @@
 import { TFolder, Vault } from 'obsidian';
 
-/**
- * Create a folderpath if it does not exist
- */
+// Create a folderpath if it does not exist
 export async function createFolder(vault: Vault, folderpath: string) {
     const folder = vault.getAbstractFileByPath(folderpath);
     if (folder && folder instanceof TFolder) {
