06edf30b84eb73c668ac43a5e2c13fd3acf6ea79
Eugene Kalinin <ekalinin@users.noreply.github.com>
2023-05-13 23:31:59 +0300

    feat: add displaying errors

--- src/settings/Settings.ts
+++ src/settings/Settings.ts
@@ -11,7 +11,7 @@ export interface TelegramSyncSettings {
     deleteMessagesFromTelegram: boolean;
     newFilesLocation: string;
   }
-  
+
   export const DEFAULT_SETTINGS: TelegramSyncSettings = {
     botToken: '',
     newNotesLocation: '',
@@ -36,7 +36,7 @@ export class TelegramSyncSettingTab extends PluginSettingTab {
         containerEl.createEl("p", { text: "Created by " }).createEl("a", {
           text: "soberHacker🍃🧘💻",
           href: "https://github.com/soberhacker",
-        });        
+        });
 
         const botFatherSetting = new Setting(containerEl)
         .setName('Bot Token')
@@ -54,7 +54,7 @@ export class TelegramSyncSettingTab extends PluginSettingTab {
         // add link to botFather
         const botFatherLink = document.createElement("a");
         botFatherLink.href = "https://t.me/botfather";
-        botFatherLink.text = "@botFather";        
+        botFatherLink.text = "@botFather";
         botFatherSetting.descEl.appendChild(botFatherLink);
 
         new Setting(containerEl)
@@ -117,22 +117,22 @@ export class TelegramSyncSettingTab extends PluginSettingTab {
         .addToggle((toggle) => {
             toggle.setValue(this.plugin.settings.deleteMessagesFromTelegram);
             toggle.onChange(async (value) => {
-            this.plugin.settings.deleteMessagesFromTelegram = value;
-            await this.plugin.saveSettings();
+              this.plugin.settings.deleteMessagesFromTelegram = value;
+              await this.plugin.saveSettings();
             });
         });
-        
+
         containerEl.createEl("hr");
 
         const donationDiv = containerEl.createEl("div", {
             cls: "telegramSyncSettingsDonationSection",
           });
-      
+
           const donationText = createEl("p");
           donationText.appendText(
             "If you like this Plugin and are considering donating to support continued development, use the buttons below!"
           );
-      
+
           donationDiv.appendChild(donationText);
           donationDiv.appendChild(
             paypalButton("https://www.paypal.com/donate/?hosted_button_id=VYSCUZX8MYGCU")
@@ -141,20 +141,20 @@ export class TelegramSyncSettingTab extends PluginSettingTab {
             buyMeACoffeeButton("https://www.buymeacoffee.com/soberhacker")
           );
           donationDiv.appendChild(kofiButton("https://ko-fi.com/soberhacker"));
-        
+
     }
 }
 
 const buyMeACoffeeButton = (link: string): HTMLElement => {
     const a = createEl("a");
-    a.setAttribute("href", link);    
+    a.setAttribute("href", link);
     a.innerHTML = `<img src="https://img.buymeacoffee.com/button-api/?text=Buy%20me%20a%20banana&emoji=🍌&slug=soberhacker&button_colour=FF5F5F&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00" height="42px"> `;
     return a;
   };
-  
+
   const paypalButton = (link: string): HTMLElement => {
     const a = createEl("a");
-    a.setAttribute("href", link);    
+    a.setAttribute("href", link);
     a.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="150" height="40">
     <path fill="#253B80" d="M46.211 6.749h-6.839a.95.95 0 0 0-.939.802l-2.766 17.537a.57.57 0 0 0 .564.658h3.265a.95.95 0 0 0 .939-.803l.746-4.73a.95.95 0 0 1 .938-.803h2.165c4.505 0 7.105-2.18 7.784-6.5.306-1.89.013-3.375-.872-4.415-.972-1.142-2.696-1.746-4.985-1.746zM47 13.154c-.374 2.454-2.249 2.454-4.062 2.454h-1.032l.724-4.583a.57.57 0 0 1 .563-.481h.473c1.235 0 2.4 0 3.002.704.359.42.469 1.044.332 1.906zM66.654 13.075h-3.275a.57.57 0 0 0-.563.481l-.145.916-.229-.332c-.709-1.029-2.29-1.373-3.868-1.373-3.619 0-6.71 2.741-7.312 6.586-.313 1.918.132 3.752 1.22 5.031.998 1.176 2.426 1.666 4.125 1.666 2.916 0 4.533-1.875 4.533-1.875l-.146.91a.57.57 0 0 0 .562.66h2.95a.95.95 0 0 0 .939-.803l1.77-11.209a.568.568 0 0 0-.561-.658zm-4.565 6.374c-.316 1.871-1.801 3.127-3.695 3.127-.951 0-1.711-.305-2.199-.883-.484-.574-.668-1.391-.514-2.301.295-1.855 1.805-3.152 3.67-3.152.93 0 1.686.309 2.184.892.499.589.697 1.411.554 2.317zM84.096 13.075h-3.291a.954.954 0 0 0-.787.417l-4.539 6.686-1.924-6.425a.953.953 0 0 0-.912-.678h-3.234a.57.57 0 0 0-.541.754l3.625 10.638-3.408 4.811a.57.57 0 0 0 .465.9h3.287a.949.949 0 0 0 .781-.408l10.946-15.8a.57.57 0 0 0-.468-.895z"></path>
     <path fill="#179BD7" d="M94.992 6.749h-6.84a.95.95 0 0 0-.938.802l-2.766 17.537a.569.569 0 0 0 .562.658h3.51a.665.665 0 0 0 .656-.562l.785-4.971a.95.95 0 0 1 .938-.803h2.164c4.506 0 7.105-2.18 7.785-6.5.307-1.89.012-3.375-.873-4.415-.971-1.142-2.694-1.746-4.983-1.746zm.789 6.405c-.373 2.454-2.248 2.454-4.062 2.454h-1.031l.725-4.583a.568.568 0 0 1 .562-.481h.473c1.234 0 2.4 0 3.002.704.359.42.468 1.044.331 1.906zM115.434 13.075h-3.273a.567.567 0 0 0-.562.481l-.145.916-.23-.332c-.709-1.029-2.289-1.373-3.867-1.373-3.619 0-6.709 2.741-7.311 6.586-.312 1.918.131 3.752 1.219 5.031 1 1.176 2.426 1.666 4.125 1.666 2.916 0 4.533-1.875 4.533-1.875l-.146.91a.57.57 0 0 0 .564.66h2.949a.95.95 0 0 0 .938-.803l1.771-11.209a.571.571 0 0 0-.565-.658zm-4.565 6.374c-.314 1.871-1.801 3.127-3.695 3.127-.949 0-1.711-.305-2.199-.883-.484-.574-.666-1.391-.514-2.301.297-1.855 1.805-3.152 3.67-3.152.93 0 1.686.309 2.184.892.501.589.699 1.411.554 2.317zM119.295 7.23l-2.807 17.858a.569.569 0 0 0 .562.658h2.822c.469 0 .867-.34.939-.803l2.768-17.536a.57.57 0 0 0-.562-.659h-3.16a.571.571 0 0 0-.562.482z"></path>
@@ -165,10 +165,10 @@ const buyMeACoffeeButton = (link: string): HTMLElement => {
     </svg>`;
     return a;
   };
-  
+
   const kofiButton = (link: string): HTMLElement => {
     const a = createEl("a");
-    a.setAttribute("href", link);    
+    a.setAttribute("href", link);
     a.innerHTML = `<img src="https://storage.ko-fi.com/cdn/brandasset/logo_white_stroke.png?" width="145" height="47">`;
     return a;
   };
