c9a98c7ee8f2cb29bb6ab1275552217c08260fe2
Burtasov <sober.guruhacker@gmail.com>
2023-05-16 23:33:48 +0100

    ci: change crlf to lf

--- install-plugin.mjs
+++ install-plugin.mjs
@@ -1,24 +1,24 @@
-import fs from 'fs';
-import path from 'path';
-import manifest from './manifest.json' assert { type: 'json' };
+import fs from "fs";
+import path from "path";
+import manifest from "./manifest.json" assert { type: "json" };
 
 const destinationFolder = process.argv[2];
 
 if (!destinationFolder) {
-  console.error('Please provide path to Obsidian Vault');
-  process.exit(1);
+	console.error("Please provide path to Obsidian Vault");
+	process.exit(1);
 }
 
-const pluginFolder = path.join(destinationFolder, '.obsidian', 'plugins', manifest.id);
+const pluginFolder = path.join(destinationFolder, ".obsidian", "plugins", manifest.id);
 
 if (!fs.existsSync(pluginFolder)) {
-  fs.mkdirSync(pluginFolder, { recursive: true });
+	fs.mkdirSync(pluginFolder, { recursive: true });
 }
 
-const filesToCopy = ['main.js', 'styles.css', 'manifest.json'];
+const filesToCopy = ["main.js", "styles.css", "manifest.json"];
 
-filesToCopy.forEach(file => {
-  fs.copyFileSync(file, path.join(pluginFolder, file));
+filesToCopy.forEach((file) => {
+	fs.copyFileSync(file, path.join(pluginFolder, file));
 });
 
-console.log('Plugin installed successfully!');
+console.log("Plugin installed successfully!");
