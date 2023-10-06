5e2df1038a7fb26c599c6d0b457ec24cc3af3fdb
Burtasov <sober.guruhacker@gmail.com>
2023-10-06 21:06:20 +0100

    update release notes

--- release-notes.mjs
+++ release-notes.mjs
@@ -1,33 +1,23 @@
 import { compareVersions } from "compare-versions";
 
 export const version = "2.0.0";
-// --------ROAD-MAP----------
+// TODO notify in setting that new beta version is ready for installing
 // TODO logging what distribution rule was selected
 // TODO getting messages one by one instead of parallel processing
-// TODO add possibility to change appending order
 // TODO add messagesLeftCnt displaying in status bar
 // TODO translating messages
-// --------LONG-AWAITED--------
-// TODO getting messages older than 24 hours
-// TODO getting messages from other bots in group chats
-// TODO post messages in selected chats
 // TODO add Demo gif and screenshots to readme.md
 // ## Demo gif example
 //![](https://raw.githubusercontent.com/vslinko/obsidian-outliner/main/demos/demo1.gif)<br>
 export const showNewFeatures = true;
-export let showBreakingChanges = true;
+export let showBreakingChanges = false;
 
 const newFeatures =
-	"This release introduces the long-awaited feature of Path Templates. " +
-	"This feature will help personalize your experience in many many ways, including:\n" +
-	" - customizing note and file names\n" +
-	" - appending data to any periodic notes (daily notes, etc)\n" +
-	" - distributing messages across different folders\n" +
-	"Many thanks to all contributors, especially to @ro_mashaaa, @fokinevgenij!";
+	"This release introduces the capability to modify both the order in which messages are appended and the delimiter used between messages when they are appended to a single note";
 export const breakingChanges =
 	"⚠️ <b><i>Due to breaking changes, it is better to check all plugin settings again! Apologies</i></b> ⚠️";
 
-export const telegramChannelLink = "https://t.me/obsidian_telegram_sync_insider";
+export const telegramChannelLink = "https://t.me/+FJ2zDTyyFM0yMGZk";
 const telegramChannelAHref = `<a href='${telegramChannelLink}'>channel</a>`;
 const telegramChannelIntroduction = `Subscribe to the plugin's ${telegramChannelAHref} to not only silence these informational messages in your bot, but also to be the first to get all the latest updates and a complete list of new features.`;
 const telegramChatLink = "<a href='https://t.me/ObsidianTelegramSync'>chat</a>";
